d02aa87ba5fe36b1f866773597361a3c39efecf8
YJH <kerspective@gmail.com>
2025-12-01 12:19:49 +0900

    배너 위치 위로 오게

--- client/src/components/WelcomeScreen.tsx
+++ client/src/components/WelcomeScreen.tsx
@@ -132,7 +132,7 @@ export function WelcomeScreen({ categories, onSearch, onCategorySelect }: Welcom
         </div>
 
         {/* Right Side - Banners */}
-        <div className="hidden xl:flex flex-col gap-6 w-[300px] fixed right-8 top-1/2 transform -translate-y-1/2">
+        <div className="hidden xl:flex flex-col gap-6 w-[300px] fixed right-8 top-32">
           {/* Today's Meal Banner */}
           <CollapsibleBanner
             title="오늘의 학식"
